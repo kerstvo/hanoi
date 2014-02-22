4bae4d99f91ce4d38df8ba0e750b795f79d17c28
Kirill Maramygin <kirill.maramygin@gmail.com>
2014-02-22 19:09:20 +0400

    init

---
+++ hanoy.js
@@ -0,0 +1,72 @@
+var Hanoy = function(el, cnt){
+	var columns_selector		= '[data-type="column"]', 
+		circle_selector			= '[data-type="circle"]',
+		columns					= el.find(columns_selector),
+		tower_h					= el.find('[data-type="tower"]').height(),
+		min_width				= 20,
+		count					= cnt;
+
+	var init = function(){
+		columns.sortable({
+			connectWith: columns_selector,
+			items: '> [data-sortable]',
+			beforeStop: function( event, ui ){
+				var circles = ui.item.siblings().filter(circle_selector);
+				// debugger;
+				if (ui.item.data('sortable') === false){
+					return false;
+				}
+				if (circles.length > 0){
+					// prevent putting one circle before existing
+					if (ui.item.prevAll(circle_selector).length > 0){
+						return false;
+					}
+					// prevent putting one circle on wider other
+					if(ui.item.nextAll(circle_selector).data('width') < ui.item.data('width')){
+						return false;
+					}
+				}
+			},
+		});
+	};
+
+	var generate_circles = function(){
+		var delta				= (100 - min_width) / (count - 1),
+			circle_h			= tower_h / count,
+			generated_circles	= "";
+
+		// columns.css('min-height',(circle_h)+'px');
+
+		for(var i = min_width; i <= 101; i += delta){
+			generated_circles += '<tr class="circle-wrapper" data-type="circle" data-width="'+i+'" data-sortable="false" style="height:'+circle_h+'px;"><td class="circle" style="width: '+i+'%;">'+i+'</td></tr>';
+		}
+
+		columns.html("<tr><td></td></tr>");
+
+		columns.first().append(generated_circles);
+		renew_sortable();
+	};
+
+	var renew_sortable = function(){
+		columns.each(function(i){
+			$(this).find(circle_selector).data('sortable', false);
+			$(this).find(circle_selector).first().data('sortable', true);
+		});
+	};
+
+	columns.on("sortstop", function( event, ui ){
+		renew_sortable();
+	});
+
+	generate_circles(3);
+	init();
+};
+
+var hanoy = new Hanoy($('[data-type=Hanoy]'), 3);
+
+
+$('#hanoy-start').on('click',function(){
+	var crcl_cnt = $('#inputCircles-Count').val();
+
+	hanoy = new Hanoy($('[data-type=Hanoy]'), crcl_cnt);
+});
